b27855e44787a9b15dfb5b1a6bea34982224298e
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-03 08:47:27 +0600

    Action Creators

--- src/store.js
+++ src/store.js
@@ -38,18 +38,43 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-store.dispatch({ type: "account/withdraw", payload: 200 });
-console.log(store.getState());
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: {
-    amount: 1000,
-    purpose: "Buy a Car",
-  },
-});
-console.log(store.getState());
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: {
+//     amount: 1000,
+//     purpose: "Buy a Car",
+//   },
+// });
+// console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+function deposite(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: {
+      amount,
+      purpose,
+    },
+  };
+}
+
+store.dispatch(deposite(500));
+store.dispatch(withdraw(200));
+
+store.dispatch(requestLoan(1000, "buy a car"));
+
 console.log(store.getState());
